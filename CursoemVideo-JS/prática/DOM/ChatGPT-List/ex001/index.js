02d30a1fcb01a6c5be5b7b5b8c2eb9316433a6b3
Eduardo Tenente <eduardodstenente@gmail.com>
2026-01-25 22:48:22 -0300

    Exercitando o DOM

---
+++ CursoemVideo-JS/prática/DOM/ChatGPT-List/ex001/index.js
@@ -0,0 +1,7 @@
+
+var tn1 = document.querySelector('input#num1')
+var tn2 = document.querySelector('input#num2')
+var r = document.querySelector('input#res')
+var n1 = Number(tn1.value)
+var n2 = Number(tn2.value)
+var S = n1 + n2
