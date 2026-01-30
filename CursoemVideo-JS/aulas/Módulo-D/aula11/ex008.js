7bfa36050e7d66d54c259af33c5512c5699c183f
Eduardo Tenente <eduardodstenente@gmail.com>
2026-01-30 10:42:39 -0300

    Aprendendo IF e ELSE

---
+++ CursoemVideo-JS/aulas/Módulo-D/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 50
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade permitida. MULTADO!')
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
