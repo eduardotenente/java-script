837f5754c608a59bdb6204a75c93b2447f4ed2b2
Eduardo Tenente <eduardodstenente@gmail.com>
2026-01-30 09:42:41 -0300

    Praticando funções interativas

--- CursoemVideo-JS/prática/DOM/ChatGPT-List/ex001/index.js
+++ CursoemVideo-JS/prática/DOM/ChatGPT-List/ex001/index.js
@@ -1,7 +1,12 @@
-
-var tn1 = document.querySelector('input#num1')
-var tn2 = document.querySelector('input#num2')
 var r = document.querySelector('input#res')
-var n1 = Number(tn1.value)
-var n2 = Number(tn2.value)
-var S = n1 + n2
+r.addEventListener('click', somar)
+
+function somar() {
+    var tn1 = document.querySelector('input#num1')
+    var tn2 = document.querySelector('input#num2')
+    var RE = document.querySelector('p#resul')
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var S = n1 + n2
+    RE.innerHTML = `A soma entre ${n1} e ${n2} é igual a ${S}`
+}
