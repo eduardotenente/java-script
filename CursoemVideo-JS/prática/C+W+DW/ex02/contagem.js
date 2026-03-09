aefd76c2010d734f83fe574524e369313c562866
Eduardo Tenente <eduardodstenente@gmail.com>
2026-03-09 09:28:23 -0300

    Exercitando

---
+++ CursoemVideo-JS/prática/C+W+DW/ex02/contagem.js
@@ -0,0 +1,9 @@
+/* Utilize um while para mostrar os números de 1 até 10. Mostre apenas os números pares. */
+
+var c = 1
+while (c <= 10) {
+    if (c % 2 == 0) {
+        console.log(c)
+    }
+    c++
+}
