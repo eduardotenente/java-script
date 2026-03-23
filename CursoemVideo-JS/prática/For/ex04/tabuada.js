6108801a0dbfd7bf6f583669f5739ff90fadd67b
Eduardo Tenente <eduardodstenente@gmail.com>
2026-03-23 10:06:46 -0300

    Exercitando for

---
+++ CursoemVideo-JS/prática/For/ex04/tabuada.js
@@ -0,0 +1,8 @@
+var data = new Date()
+var agora = data.getHours()
+console.log(`Exibindo a tabuada ás ${agora} horas...`)
+for (var c = 1; c <= 10; c++) {
+    var Res = 1 * c
+    console.log(`1 x ${c} = ${Res}`)
+}   
+console.log('FIM')
