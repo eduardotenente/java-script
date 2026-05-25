ba77dc320ce6ff878f117849e4c5643dc047eff6
Eduardo Tenente <eduardodstenente@gmail.com>
2026-05-24 23:36:54 -0300

    Exercitando javascript

--- CursoemVideo-JS/prática/exercicios-pdf/ex063/numeros.js
+++ CursoemVideo-JS/prática/exercicios-pdf/ex063/numeros.js
@@ -7,7 +7,41 @@
     c) A média entre todos os valores 
     d) Quantos valores são pares
 */
+var resposta = 1
+var soma = 0
+var menor = Infinity
+var maior = 0 // Exercício EXTRA
+var Par = 0
+var Impar = 0 // Exercício EXTRA
+let c = 1
+
 do {
-    console.log('Sim')
-    let resposta = 'N'
-} while(resposta = 'S')
+    let num = Math.floor(Math.random() * 100 )
+    console.log('---------------------------')
+    console.log(`${c}º número: ${num}`)
+    var soma = soma + num
+    if(num < menor) {
+        var menor = num
+    }
+    if(num > maior) {
+        var maior = num
+    }
+    if(num % 2 == 0) {
+        Par++
+    } else {
+        Impar++
+    }
+    resposta++
+    c++
+} while(resposta < 10)
+let média = soma / c
+
+console.log('---------------------------')
+console.log('EXIBINDO RESULTADOS...')   
+console.log()
+console.log(`Somatório dos números: ${soma}`)
+console.log(`Maior número: ${maior}`) // EXTRA
+console.log(`Menor número: ${menor}`)
+console.log(`Média dos números: ${média}`)
+console.log(`Quantidade de números pares: ${Par}`)
+console.log(`Quantidade de números impares: ${Impar}`) // EXTRA
