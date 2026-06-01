249906c91b446637c77672b31b503109924c821e
Eduardo Tenente <eduardodstenente@gmail.com>
2026-06-01 10:04:18 -0300

    praticando vetores com números aleatórios

--- CursoemVideo-JS/prática/exercicios-pdf/ex082/notas_vetor.js
+++ CursoemVideo-JS/prática/exercicios-pdf/ex082/notas_vetor.js
@@ -7,3 +7,37 @@
     d) Em que posições a maior nota aparece
 */
 
+let notas = [0, 0, 0, 0, 0, 0, 0, 0]
+let c = 1
+var soma = 0
+var AcimadaMédia = 0
+var maior = 0
+var posição = 0
+
+console.log('---------- Notas -----------')
+for(let pos in notas) {
+    notas[pos] = Math.floor(Math.random() * 10)
+    console.log(`  Aluno ${c}:   ${notas[pos]}`)
+    soma = soma + notas[pos]
+    if(notas[pos] > maior) {
+        maior = notas[pos]
+        posição = c
+    }
+    c++
+}
+console.log('---------- Notas -----------')
+
+let média = soma / c
+for(let pos in notas) {
+    if(notas[pos] > média) {
+        AcimadaMédia++
+    }
+}
+
+console.log()
+console.log('---------- Resultados ----------')
+console.log(`A média da turma é: ${média}`)
+console.log(`Alunos acima da média da turma: ${AcimadaMédia}`)
+console.log(`Maior nota: ${maior}`)
+console.log(`Posição da maior nota: ${posição}`)
+console.log('--------------------------------')
