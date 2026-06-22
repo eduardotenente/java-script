ecadd190e9fde0f01974e86a96959789ecf5c189
Eduardo Tenente <eduardodstenente@gmail.com>
2026-06-22 10:32:46 -0300

    Maior, menor valor e somatório

--- CursoemVideo-JS/exercícios-propostos/Módulo-F-exp/ambiente.js
+++ CursoemVideo-JS/exercícios-propostos/Módulo-F-exp/ambiente.js
@@ -2,6 +2,10 @@ var num = document.getElementById('fnum')
 var lista = document.getElementById('resvalor')
 let res = document.querySelector('div#res')
 let valores = [num]
+let maior = 0
+let menor = Infinity
+var soma = 0
+var c = 0
 
 function isNumero(n) {
     if(Number(n) >= 1 && Number(n) <= 100) {
@@ -20,13 +24,19 @@ function isLista(n, l) {
 }
 
 function Adicionar() {
-    var c = 0
     if (isNumero(num.value) && !isLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
         lista.appendChild(item)
         c++
+        if(num.value > maior) {
+            maior = num.value
+        }
+        if(num.value < menor) {
+            menor = num.value
+        }
+        soma = soma + num.value
     } else {
         alert('Valor Inválido ou já encontrado na lista!')
     }
@@ -40,6 +50,10 @@ function Finalizar() {
     } else {
         let tot = valores.length
         res.innerHTML = ''
-        res.innerHTML = `<p> Ao todo, temos ${tot} números cadastrados </p>`
+        res.innerHTML = `<p> Ao todo, temos ${tot- 1} números cadastrados </p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior}. </p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor}. </p>`
+        res.innerHTML += `<p> Somando todos os valores, temos ${Number(soma.value)}. </p>`
+        res.innerHTML += `<p> A média dos valores digitados é ${média.value} .</p>`
     }
 }
